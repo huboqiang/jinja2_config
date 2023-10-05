384fc151e61ab6ccd461657dba388fc93ab3ab5b
bqhu_jh <huboqiang@zju.edu.cn>
2023-10-05 19:03:18 +0800

    support jhuang

--- jinja_process.py
+++ jinja_process.py
@@ -2,15 +2,32 @@ import sys
 from jinja2 import Template
 import yaml
 
-def _load_color_config(file_config):
+
+def _load_color_config(file_config, sep):
     # 读取模板文件内容
     with open(file_config, 'r') as f:
         template_string = f.read()
 
     template = Template(template_string)
     data_ = yaml.load(template_string.split("\nproject:")[0], Loader=yaml.FullLoader)
-
-    filled_template = template.render(colors=data_["colors"])
+    filled_template = template.render(colors=data_[sep])
     return filled_template
 
-print(_load_color_config(sys.argv[1]))
+def _convert_jh(template):
+    data = yaml.load(template, Loader=yaml.FullLoader)
+    key = "project"
+    for project in data[key]:
+        for dataset in data[key][project]:
+            dict_new = {
+                "default": {"project_name": project, "dataset": dataset},
+                "colors": data[key][project][dataset]
+            }
+            print(yaml.dump(dict_new, default_flow_style=False, sort_keys=False))
+
+
+
+sep = "colors"
+if len(sys.argv) > 2:
+    sep = sys.argv[2]
+
+_convert_jh(_load_color_config(sys.argv[1], sep))
